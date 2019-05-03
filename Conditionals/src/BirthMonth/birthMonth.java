bfc836646ba820a91a31ad2deacf8f690fed0240
Stephen <38361922+stephen-eades@users.noreply.github.com>
2019-05-03 00:44:31 -0400

    Update birthMonth.java

--- Conditionals/src/BirthMonth/birthMonth.java
+++ Conditionals/src/BirthMonth/birthMonth.java
@@ -1,4 +1,5 @@
 package mypackage;
+
 import java.util.Scanner;
 
 public class birthMonth 
@@ -6,41 +7,42 @@ public class birthMonth
 
 	public static void main(String[] args) 
 	{
-		
 		// Variables
 		Scanner in = new Scanner(System.in);
 		int userBirthMonth = 0;
-		String trash = ""; // use for bad input which will read as a String
+		String trash = "";
 		
-			// Prompt and input
+			// Prompt and check for an int
 			System.out.println("Please enter your birth month (1-12)");
-			if(in.hasNextInt()) // OK safe to read in an Int
+			if(in.hasNextInt())
 			{
-			    // Get birth month
-			    userBirthMonth = in.nextInt(); 
+				// if int, assign to var
+			    	userBirthMonth = in.nextInt(); 
 				
-					if (userBirthMonth >= 1 && userBirthMonth <= 12)// test if number is in range 1-12
+					// test if number is in range 1-12
+					if (userBirthMonth >= 1 && userBirthMonth <= 12)
 					{
-						System.out.println("Your birth month is: " + userBirthMonth); // echo
-						System.exit(0);  // terminate the program
+						System.out.println("Your birth month is: " + userBirthMonth);
+						System.exit(0);
 					}
 					else
 					{
-					    System.out.println("You entered an incorrect month value: " + userBirthMonth); // echo
-					    System.out.println("Please restart and use a number between 1-12 inclusive");
-						System.exit(0);  // terminate the program
+					   	System.out.println("You entered an incorrect month value: " + userBirthMonth);
+					    	System.out.println("Please restart and use a number between 1-12 inclusive");
+						System.exit(0);
 					}
 			}
 			else
 			{ 
-			   // Not an int can’t use nextInt() read as String with nextLine() instead
-			   trash = in.nextLine();
-			   System.out.println("\nYou said your birth month was: " + trash);
-			   System.out.println("Run the program again and enter a valid month number (1-12)!");
-			   System.exit(0);  // terminate the program
+				// Not in, can’t use nextInt() so read as String with nextLine() instead
+			  	 trash = in.nextLine();
+			   	System.out.println("\nYou said your birth month was: " + trash);
+			  	 System.out.println("Run the program again and enter a valid month number (1-12)!");
+			  	 System.exit(0);  // terminate the program
 			}
-			
-		in.close();	// Closing scanner resource
+		
+		// Closing scanner resource
+		in.close();
 	}
 
 }
