14f2481f17ff7b6a26b7c8b87f62af831281fccd
Stephen <38361922+stephen-eades@users.noreply.github.com>
2019-05-07 18:31:52 -0400

    Update CtoF.java

--- Looping/src/CtoF.java
+++ Looping/src/CtoF.java
@@ -1,42 +1,46 @@
 import java.util.Scanner;
 
-public class CtoF {
+public class CtoF 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		// Variables
 		Scanner in = new Scanner(System.in);
 		int tempCelsius = 0;
 		double tempFahrenheit = 0;
 		boolean validInput = false;
-		
-			 do // loop until we get valid user input
-		   	 {
-		   		 System.out.println("Please enter your temperature in Celsius: ");
-		            tempCelsius = in.nextInt();
+			
+			// Loop until we get valid user input
+			do
+			{
+				System.out.println("Please enter your temperature in Celsius: ");
+				tempCelsius = in.nextInt();
+				
 		            	if (tempCelsius >= 0 && tempCelsius <= 100)
-		               {
-		            		 System.out.println("The temperature you entered was: " + tempCelsius); // echo valid input
-		                     validInput = true;
-		               }
-		               else
-		               {
-				    		 // Temperature was outside of ranges
-							 System.out.println("\nYou said the temperature was: " + tempCelsius);
-							 System.out.println("Please enter a Celsius temperature between the freezing (0) and boiling point(100).");
-		                     validInput = false;
-		               }
-		   	 }
-		   	 while (validInput == false);
+				{
+					// Echo valid input
+					System.out.println("The temperature you entered was: " + tempCelsius);
+		                    	validInput = true;
+		              	}
+		               	else
+		               	{
+					// Temperature was outside of ranges
+					System.out.println("\nYou said the temperature was: " + tempCelsius);
+					System.out.println("Please enter a Celsius temperature between the freezing (0) and boiling point(100).");
+		                     	validInput = false;
+		                }
+				
+		   	} while (validInput == false);
 
-		// Process input
-		System.out.println("Converting the temperature from Celsius to Fahrenheit...");
-		// Conversion process: (0°C × 9/5) + 32 = 32°F
-		tempFahrenheit = (tempCelsius * (1.8)) + 32;
-		System.out.println("Your temperature of " + tempCelsius + " converted to Fahrenheit is: " + tempFahrenheit);
+			// Process input
+			System.out.println("Converting the temperature from Celsius to Fahrenheit...");
+			// Conversion process: (0Â°C Ã— 9/5) + 32 = 32Â°F
+			tempFahrenheit = (tempCelsius * (1.8)) + 32;
+			System.out.println("Your temperature of " + tempCelsius + " converted to Fahrenheit is: " + tempFahrenheit);
 		
-		//  Closer scanner
-		in.close();
+		in.close(); //  Close scanner resource
 		
 	}
 
